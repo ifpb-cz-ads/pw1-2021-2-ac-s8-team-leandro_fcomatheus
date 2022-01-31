4ae9539c1da8b7cc24d6d3e612ade31489a33e15
Lacerda234 <70702222+Lacerda234@users.noreply.github.com>
2022-01-31 14:13:40 -0300

    Adicionando questão 01

---
+++ questão01.py
@@ -0,0 +1,9 @@
+def maximo(x, y):
+    if x > y:
+        return x
+    else:
+        return y
+
+print(f'máximo(5,6) == 6 -> resultado: {maximo(5,6)}')
+print(f'máximo(2,1) == 2 -> resultado: {maximo(2,1)}')
+print(f'máximo(7,7) == 7 -> resultado: {maximo(7,7)}')
